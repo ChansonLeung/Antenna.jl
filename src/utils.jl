f604651c0eb0c72ec4f0e4c4fb53c62208342985
Chanson <liangchangsong@163.com>
2022-03-22 22:37:02 +0800

    format

--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,4 @@
-export 
+export
     scale2log,
     normlog,
     sph2cart,
@@ -10,9 +10,9 @@ scale2log(x) = 20log10(x)
 normlog(vec) = vec .- maximum(vec)
 sph2cart(θ, ϕ, r) = begin
     (
-        x = r * sin(θ)cos(ϕ),
-        y = r * sin(θ)sin(ϕ),
-        z = r * cos(θ)
+        x=r * sin(θ)cos(ϕ),
+        y=r * sin(θ)sin(ϕ),
+        z=r * cos(θ)
     )
 end
 cart2sph(x, y, z) = begin
@@ -21,18 +21,18 @@ cart2sph(x, y, z) = begin
     θ = acos(z / r)
     ϕ = atan(y, x)
     (
-        θ = θ,
-        ϕ = ϕ,
-        r = r
+        θ=θ,
+        ϕ=ϕ,
+        r=r
     )
 end
 # convert to domain θ ∈[0°, 180°], ϕ∈ [-180°, 180°]
 # bad implementation
 mod_angle = (θ, ϕ) -> begin
-    sign = x-> x>=0 ? 1 : -1
+    sign = x -> x >= 0 ? 1 : -1
     item1 = -sign.(mod2pi.(θ) .- π)
-    θ_ = mod1.(θ.*item1 , π)
-    θ_[θ.==0] .= 0  
+    θ_ = mod1.(θ .* item1, π)
+    θ_[θ.==0] .= 0
     ϕ_ = item1 .* ϕ .|> mod2pi
-    (θ_,ϕ_)
+    (θ_, ϕ_)
 end
